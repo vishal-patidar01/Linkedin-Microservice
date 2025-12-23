e1195f7eedf7d1729193af37f2aed1dad4da1e6d
vishal patidar <vishalsuman8770@gmail.com>
2025-12-23 20:48:44 +0530

    add new mapping

--- posts-service/src/main/java/com/vishal/linkedin/posts_service/controllers/PostsController.java
+++ posts-service/src/main/java/com/vishal/linkedin/posts_service/controllers/PostsController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/core")
 @RequiredArgsConstructor
 public class PostsController {
 
